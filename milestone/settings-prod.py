3bcc2d3accf79ef3a55f65a5ff70a50b6a92b63f
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-05-06 13:09:29 +0530

    Update settings-prod.py

--- milestone/settings-prod.py
+++ milestone/settings-prod.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-3=b_*0^w%c2gu%4^jd1dc*wpww17o*v6h#)ewt!*)ybt)i(k3n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'env-5683222.in1.cloudlets.co.in',  # Add the domain causing the error
+    '127.0.0.1',  # Localhost for development
+    'localhost',
+]
 
 
 # Application definition

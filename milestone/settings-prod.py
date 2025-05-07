da4a24fae458103c3504cb628e95a842c61cb638
parthiban m <97148433+parthiban777@users.noreply.github.com>
2025-05-07 11:43:19 +0530

    Update settings-prod.py

--- milestone/settings-prod.py
+++ milestone/settings-prod.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-3=b_*0^w%c2gu%4^jd1dc*wpww17o*v6h#)ewt!*)ybt)i(k3n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'env-5683222.in1.cloudlets.co.in',  # Add the domain causing the error
-    '127.0.0.1',  # Localhost for development
-    'localhost',
-]
 
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'node223383-shinova.in1.cloudlets.co.in',
+]
 
 # Application definition
 

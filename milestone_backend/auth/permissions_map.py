d8756123be44eaef7d4dde7c9a58b234a1148004
manibalan (SMRFT) <manibalansmrft@gmail.com>
2025-12-17 15:39:15 +0530

    Update permissions_map.py

--- milestone_backend/auth/permissions_map.py
+++ milestone_backend/auth/permissions_map.py
@@ -16,7 +16,7 @@ PAGE_MAPPING = {
     '/_b_a_c_k_e_n_d/Milestone/register/':'MDC-P-REG',
     '/_b_a_c_k_e_n_d/Milestone/referrals/?(\?.*)?$':'MDC-P-SOR',
     '/_b_a_c_k_e_n_d/Milestone/therapy_billing/':'MDC-P-TRB',
-    '/_b_a_c_k_e_n_d/Milestone/update-patient/?(\?.*)?$':'MDC-P-PTE',
+    '/_b_a_c_k_e_n_d/Milestone/update-patient/.*':'MDC-P-PTE',
     '/_b_a_c_k_e_n_d/Milestone/update-doctor(?:/[^/]+)+/$':'MDC-P-RDE',
     '/_b_a_c_k_e_n_d/Milestone/update-employeeDr(?:/[^/]+)+/$':'MDC-P-CDE',
     
@@ -26,7 +26,7 @@ PAGE_MAPPING = {
     '/_b_a_c_k_e_n_d/Milestone/attendance/':'MDC-API-AT',
     r'^/_b_a_c_k_e_n_d/Milestone/all-attendance-patient/?(\?.*)?$': 'MDC-API-AT',
     '/_b_a_c_k_e_n_d/Milestone/add_patient_attendance/':'MDC-API-AT',
-    '/_b_a_c_k_e_n_d/Milestone/get_all_patient_attendance/':'MDC-API-AT',
+    r'^/_b_a_c_k_e_n_d/Milestone/get_all_patient_attendance/?(\?.*)?$': 'MDC-API-AT',
     '/_b_a_c_k_e_n_d/Milestone/edit_patient_attendance/':'MDC-API-AT',
     '/_b_a_c_k_e_n_d/Milestone/delete_patient_attendance/':'MDC-API-AT',
     "/_b_a_c_k_e_n_d/Milestone/get_all_patient_details/":'MDC-API-AT',
@@ -35,6 +35,8 @@ PAGE_MAPPING = {
     "/_b_a_c_k_e_n_d/Milestone/all-patient-filterless/":'MDC-API-PDC',
     "/_b_a_c_k_e_n_d/Milestone/HistoryRecordingSheet/":'MDC-API-PDC',
     "/_b_a_c_k_e_n_d/Milestone/GetHistoryRecordingSheet/":'MDC-API-PDC',
+    "/_b_a_c_k_e_n_d/Milestone/update_attendance_sessions/":'MDC-API-AT',
+    r'^/_b_a_c_k_e_n_d/Milestone/get_all_patient_oldattendance/?(\?.*)?$': 'MDC-API-AT',
 
     '/_b_a_c_k_e_n_d/Milestone/all-assessments/':'',
     '/_b_a_c_k_e_n_d/Milestone/login/':'',
@@ -56,6 +58,7 @@ PAGE_MAPPING = {
     '/_b_a_c_k_e_n_d/Milestone/CBCLgirlsassessment/':'',
     '/_b_a_c_k_e_n_d/Milestone/get-cbcl/':'',
     '/_b_a_c_k_e_n_d/Milestone/get-cbcl/<str:childName>/':'',
+    '/_b_a_c_k_e_n_d/Milestone/oldtherapy-reports/?(\?.*)?$':'MDC-API-THR',
 
     '/therapy-reports/':'MDC-API-THR',
     '/get_patient_assessments/':'MDC-API-GAS',
